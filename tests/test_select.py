bf38f5a94378d4e8e74ad720356e6abf7a0c7e10
Yoshida Takuto <takuto.yoshida@gmail.com>
2019-11-15 19:00:50 +0900

    add test

--- tests/test_select.py
+++ tests/test_select.py
@@ -2,6 +2,7 @@ import unittest
 from crepedb import CrepeDB
 from crepedb.models import Shop
 
+
 class TestSelect(unittest.TestCase):
     def setUp(self):
         self.db = CrepeDB('sqlite:///:memory:')
@@ -18,20 +19,22 @@ class TestSelect(unittest.TestCase):
         # sorted(['食堂','喫茶','レストラン'],reverse=True)[0] > '食堂'
         res_shops = self.db.select_shop(order_by=Shop.name, descend=True)
         self.assertEqual(res_shops[0], inserted[0])
-        
-        res_shop = self.db.select_shop(limit=1, pagenum=2) 
+
+        res_shop = self.db.select_shop(limit=1, pagenum=2)
         self.assertEqual(res_shop[0], inserted[2])
-    
+
     def test_select_shop_lazy(self):
         shops = [
             {'name': 'Crepe食堂', 'tel': '01234567890'},
             {'name': 'Crepe喫茶', 'tel': '0312345678'},
             {'name': 'Crepeレストラン', 'address': '東京都目黒区'},
         ]
-        inserted = self.db.insert_shops(shops)
+        insertedShops = self.db.insert_shops(shops)
 
-        for inserted, selected in zip(inserted, self.db.select_shop_lazy(limit=1)):
+        for inserted, selected in zip(insertedShops, self.db.select_shop_lazy(limit=1)):
             self.assertEqual(inserted, selected[0])
 
+        insertedShops.reverse()
 
-
+        for inserted, selected in zip(insertedShops, self.db.select_shop_lazy(limit=1, descend=True)):
+            self.assertEqual(inserted, selected[0])
