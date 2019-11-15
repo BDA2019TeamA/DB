cfc1e8c7c5d7adf729d55e495084c6875666745c
Yoshida Takuto <takuto.yoshida@gmail.com>
2019-11-15 12:13:14 +0900

    add insert_(site|page|review)

--- src/crepedb/insert.py
+++ src/crepedb/insert.py
@@ -1,13 +1,19 @@
 from sqlalchemy.orm import sessionmaker
 from . import models
-from .models import Shop
+from .models import Shop, Site, Page, Review
+
+
+def getParam(dic, key):
+    if key in dic:
+        return dic[key]
+    return None
 
 
 def insert_shop(session, shop):
     item = Shop()
-    item.name = shop['name'] if 'name' in shop else None
-    item.address = shop['address'] if 'address' in shop else None
-    item.tel = shop['tel'] if 'tel' in shop else None
+    item.name = getParam(shop, 'name')
+    item.address = getParam(shop, 'address')
+    item.tel = getParam(shop, 'tel')
 
     session.add(item)
     session.commit()
@@ -19,11 +25,93 @@ def insert_shops(session, shops):
     items = []
     for shop in shops:
         item = Shop()
-        item.name = shop['name'] if 'name' in shop else None
-        item.address = shop['address'] if 'address' in shop else None
-        item.tel = shop['tel'] if 'tel' in shop else None
+        item.name = getParam(shop, 'name')
+        item.address = getParam(shop, 'address')
+        item.tel = getParam(shop, 'tel')
         items.append(item)
     session.add_all(items)
     session.commit()
 
     return items
+
+
+def insert_site(session, site):
+    item = Site()
+
+    item.name = getParam(site, 'name')
+    item.url = getParam(site, 'url')
+
+    session.add(item)
+    session.commit()
+
+
+def insert_sites(session, sites):
+    items = []
+
+    for site in sites:
+        item = Site()
+
+        item.name = getParam(site, 'name')
+        item.url = getParam(site, 'url')
+        items.append(item)
+
+    session.add_all()
+    session.commit()
+
+
+def insert_page(session, page):
+    item = Page()
+
+    item.evaluation = getParam(page, 'evaluation')
+    item.url = getParam(page, 'url')
+    item.genre = getParam(page, 'genre')
+    site_id = getParam(page, 'site_id')
+    shop_id = getParam(page, 'shop_id')
+
+    session.add(item)
+    session.commit()
+
+
+def insert_pages(session, pages):
+    items = []
+    for page in pages:
+        item = Page()
+
+        item.evaluation = getParam(page, 'evaluation')
+        item.url = getParam(page, 'url')
+        item.genre = getParam(page, 'genre')
+        site_id = getParam(page, 'site_id')
+        shop_id = getParam(page, 'shop_id')
+        items.push(item)
+
+    session.add_all(items)
+    session.commit()
+
+
+def insert_review(session, review):
+    item = Review()
+
+    reviewer = getParam(review, 'reviewer')
+    comment = getParam(review, 'comment')
+    evaluation = getParam(review, 'evaluation')
+    original_id = getParam(review, 'original_id')
+    page_id = getParam(review, 'page_id')
+
+    session.add(item)
+    session.commit()
+
+
+def insert_reviews(session, reviews):
+    items = []
+
+    for review in reviews:
+        item = Review()
+
+        reviewer = getParam(review, 'reviewer')
+        comment = getParam(review, 'comment')
+        evaluation = getParam(review, 'evaluation')
+        original_id = getParam(review, 'original_id')
+        page_id = getParam(review, 'page_id')
+
+    session.add_all(items)
+    session.commit()
